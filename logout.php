d9d5c1741d499736303d3130fd5f2ef3a3262527
Sajan Vishwakarma <67305384+Sajan-Vishwakarma@users.noreply.github.com>
2020-12-14 11:15:25 +0530

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    session_unset();
-    session_destroy();
-  
-    echo "will redirect to login page in 1 seconds";
-    header('Refresh: 1; URL=user-login.php');
-?>
